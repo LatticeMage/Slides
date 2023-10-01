188f37444cdf5970fe164bcafdb7f874fc7bafe6
LatticeMage <LatticeMage@gmail.com>
2023-10-01 08:39:21 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -2,6 +2,8 @@ git config --local user.name "LatticeMage"
 git config --local user.email "LatticeMage@gmail.com"
 git remote set-url origin git@LM:LatticeMage/Slides.git
 
+git submodule update --recursive --remote
+
 git pull
 git add .
 git commit -m "upload"
