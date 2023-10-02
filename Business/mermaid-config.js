38486f39e62c9184375eeb6f5ecee14fa2cb1976
LatticeMage <LatticeMage@gmail.com>
2023-10-02 18:22:30 +0800

    upload

--- Business/mermaid-config.js
+++ Business/mermaid-config.js
@@ -1,14 +1,21 @@
-// mermaid-config.js
+// Ensure Mermaid is configured before trying to render diagrams
+mermaid.initialize({
+    startOnLoad: true,
+    theme: 'default',  // or dark, forest, etc.
+});
+
 window.addEventListener('DOMContentLoaded', (event) => {
     fetch('./Funnel.mmd')
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
         .then(data => {
-            mermaid.mermaidAPI.initialize({
-                startOnLoad: true
+            const diagram = mermaid.mermaidAPI.render('mermaid-diagram', data, (svgCode) => {
+                document.getElementById('mermaid-diagram').innerHTML = svgCode;
             });
-
-            const diagram = mermaid.mermaidAPI.render('mermaid-diagram', data);
-            document.getElementById('mermaid-diagram').innerHTML = diagram;
         })
         .catch(error => console.error('Error fetching the diagram file:', error));
 });
