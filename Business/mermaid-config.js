5dee5ba6807f0affd41c7c59809ad872067f0577
LatticeMage <LatticeMage@gmail.com>
2023-10-02 18:14:47 +0800

    upload

--- Business/mermaid-config.js
+++ Business/mermaid-config.js
@@ -3,10 +3,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     fetch('./Funnel.mmd')
         .then(response => response.text())
         .then(data => {
-            mermaid.initialize({startOnLoad:true});
-            mermaid.render('mermaid-diagram', data, (svgCode) => {
-                document.getElementById('mermaid-diagram').innerHTML = svgCode;
+            mermaid.mermaidAPI.initialize({
+                startOnLoad: true
             });
+
+            const diagram = mermaid.mermaidAPI.render('mermaid-diagram', data);
+            document.getElementById('mermaid-diagram').innerHTML = diagram;
         })
         .catch(error => console.error('Error fetching the diagram file:', error));
 });
