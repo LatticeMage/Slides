d82498fe7abe64148fb37075223d28465f7a9165
LatticeMage <LatticeMage@gmail.com>
2024-02-03 14:30:12 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -1,6 +1,11 @@
-cd _shared
+cd _includes
 ./upload.bat
 cd ../
+
+cd _layouts
+./upload.bat
+cd ../
+
 
 git config --local user.name "LatticeMage"
 git config --local user.email "LatticeMage@gmail.com"
