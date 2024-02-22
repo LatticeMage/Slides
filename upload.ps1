2c3780ad64e25d7d26a64b7532f955ad07adbba8
LatticeMage <LatticeMage@users.noreply.github.com>
2024-02-22 21:26:09 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -3,12 +3,16 @@ cd _includes
 cd ../
 
 cd _layouts
+./upload.ps1
+cd ../
+
+cd assets
 ./upload.bat
 cd ../
 
 
 git config --local user.name "LatticeMage"
-git config --local user.email "LatticeMage@gmail.com"
+git config --local user.email "LatticeMage@users.noreply.github.com"
 git remote set-url origin git@LM:LatticeMage/Slides.git
 
 git submodule update --recursive --remote
